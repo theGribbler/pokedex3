ccddaba3245cf88498798f7f7c4a9b9977bc4666
Paul Lathrop <pclathrop@gmail.com>
2017-07-06 16:00:53 -0700

    Up to the Detail View Images and stuff too

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Paul Lathrop on 7/3/17.
+//  Copyright © 2017 Paul Lathrop. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Paul Lathrop on 7/3/17.
+//  Copyright © 2017 Paul Lathrop. All rights reserved.
+//
+
+import Foundation
