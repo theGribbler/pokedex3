f3cb04dc6e503fc5607f88bf3cd57a0a470eee8b
Paul Lathrop <pclathrop@gmail.com>
2017-07-07 14:36:50 -0700

    music and the detail view

--- pokedex3/PokeCell.swift
+++ pokedex3/PokeCell.swift
@@ -10,4 +10,24 @@ import UIKit
 
 class PokeCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0 //rounded corners in cell
+    }
+    
+    func configureCell(_ pokemon: Pokemon){
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+        
+    }
 }
--- pokedex3/Pokemon.swift
+++ pokedex3/Pokemon.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+class Pokemon {
+
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String{
+        return _name
+    }
+    
+    var pokedexId: Int{
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
+
+
